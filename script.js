0825e95ff203454e7fe48385bd6b3dcc11286d02
yutami <sabacan623@gmail.com>
2023-12-06 01:06:41 +0900

    コード整形

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+const drawBall = () => {
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
   x += dx;
   y += dy;
 }
